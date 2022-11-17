a91016753c05981ef7183d34652a334e9ab28930
Ito Masaki <ito.masaki@em.ci.ritsumei.ac.jp>
2022-11-17 20:06:19 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
 		  <div className="Twitch-view">
-		    <div className="Pac-man"/>
+		    <div className="Pac-man" />
 		  </div>
 		  
 		  <div className="Control-panel">
