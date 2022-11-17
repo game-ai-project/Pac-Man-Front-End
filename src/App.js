a20b811786c688060f881da0514904db118e3929
Ito Masaki <ito.masaki@em.ci.ritsumei.ac.jp>
2022-11-17 12:10:44 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+		  <div className="View-of-Twitch" />
+		  <div className="Instruction" />
+		  <div className="Audio-settings" />
+		  <div className="Action-log" />
+		  <div className="Record" />
     </div>
   );
 }
